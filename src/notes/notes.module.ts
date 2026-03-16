602ad6f91666dfaae1bd6ecdacddecba29926d1a
hasinhafiz <hasinhafiz1@gmail.com>
2026-03-16 13:30:45 +0530

    Configure TypeORM features

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './notes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Note } from './note.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Note])],
   providers: [NotesService]
 })
-export class NotesModule {}
+export class NotesModule { }
