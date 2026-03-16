4a2536a60eae33f11d1b9eb79f61d98cf40fd143
hasinhafiz <hasinhafiz1@gmail.com>
2026-03-16 14:50:01 +0530

    feat: implement signup endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private usersService: UsersService,
+  ) { }
   @Post('/signup')
   signUp(@Body() body: CreateUserDto) {
-    return body;
+    return this.usersService.create(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) { }
+  create(userObject: CreateUserDto) {
+    const user = this.repo.create(userObject); // Creates new entity
+    return this.repo.save(user); // Saves created entity to repository
+  }
+}
